62184ec7c57f61e10df021d06d333a386bb7e458
kairemerson <kairemerson@gmail.com>
2021-02-14 17:38:30 -0300

    logica do botao start e pause implementada

--- js/script.js
+++ js/script.js
@@ -7,19 +7,29 @@ var delay = 1000
 function start(){
     cron = setInterval(()=>{
         timer()
-        console.log("start");
     },delay)
 }
 
 function pause(){
-    console.log("pause");
+    clearInterval(cron)
 }
 
 function stop(){
+    clearInterval(cron)
     console.log("stop");
 }
 
 function timer(){
-    var format = "00:00:11"
-    document.querySelector(".display").innerHTML = format
+    segundo++
+    if(segundo == 60){
+        segundo = 0
+        minuto++
+        if(minuto == 60){
+            minuto = 0
+            hora++
+        }
+    }
+    var formatada = `
+    ${(hora < 10 ? "0" + hora : hora)} : ${(minuto < 10 ? "0" + minuto : minuto)} : ${(segundo < 10 ? "0" + segundo : segundo)}`
+    document.querySelector(".display").innerHTML = formatada
 }
